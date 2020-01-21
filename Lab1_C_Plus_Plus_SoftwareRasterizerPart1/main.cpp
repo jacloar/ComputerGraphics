0cd7a7fa6e2395b5972b66f70fecc7bdc11b8c3e
dmuller189 <muller.d@husky.neu.edu>
2020-01-21 00:41:18 -0500

    triangle filling

--- Lab1_C_Plus_Plus_SoftwareRasterizerPart1/main.cpp
+++ Lab1_C_Plus_Plus_SoftwareRasterizerPart1/main.cpp
@@ -16,6 +16,8 @@
 
 // Load our libraries
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 // Some define values
 #define WINDOW_HEIGHT 320
@@ -32,7 +34,7 @@
 
 // Create a canvas to draw on.
 TGA canvas(WINDOW_WIDTH,WINDOW_HEIGHT);
-
+bool validPixel(int x, int y, Vec2 v0, Vec2 v1, Vec2 v2);
 
 // Implementation of Bresenham's Line Algorithm
 // The input to this algorithm is two points and a color
@@ -64,44 +66,92 @@ void drawLine(Vec2 v0, Vec2 v1, TGA& image, ColorRGB c){
 
 
 
+
+
 // Draw a triangle
 void triangle(Vec2 v0, Vec2 v1, Vec2 v2,TGA& image, ColorRGB c){
-    if(glFillMode==LINE){
+   
+    if(glFillMode==LINE || true){
         drawLine(v0,v1,image,c);
         drawLine(v1,v2,image,c);
         drawLine(v2,v0,image,c);
     }
 
     // TODO: Draw a filled triangle
+    std::vector<Vec2> triVec;
 
-    //A 
-    //steps (naive brute force):
-    // 1) create view box with bounds of triange
-    // 2) iterate over all pixels of view box
-    // 3) if pixel is inside triangle, set the pixel color
-    // 4) else, do nothing -
+    triVec.push_back(v0);
+    triVec.push_back(v1);
+    triVec.push_back(v2);
+
+    //boundaries to iterate over box to fill
+    int left = v0.x; //leftmost x value to be set
+    int right = v0.x; //rightmost x value to be set
+    int top = v0.y; //uppermost y value to be set
+    int bottom = v0.y; //uppermost y value to be set
+
+    for (Vec2 &v : triVec) {
+        if (v.x < left) {
+            left = v.x;
+        }
+        if (v.x > right) {
+            right = v.x;
+        }
+        if (v.y > bottom) {
+            bottom = v.y;
+        }
+        if (v.y < top) {
+            top = v.y;
+        }
+    }
 
 
-    //B 
-    //more clever (like bfs)
-    //start at vertex
-    //move to adjacent 6 pixels
-    //for each adjacent pixel:
-    //  if pixel is inside triangle and un filled, color it and recur
-    //  else, do nothing
+    std::cout << std::endl << "left is " << left << std::endl;
+    std::cout << "right is " << right << std::endl;
+    std::cout << "bottom is " << bottom << std::endl;
+    std::cout << "top is " << top << std::endl;
 
-    //C
-    //start at leftmost x coordinate
-    //traverse left to right for each pixel until reached right most pixel
-    //for each x-pixel, traverse on y axis and fill pixels inside triangle
 
-    //D
-    //find mid point of triangle and draw a line ot all edges
-    // this creates three new triangles
-    // do the same on each triangle
+    if (true) {
+
+        for (int row = top; row <= bottom; row++) {
+            for (int col = left; col <= right; col++) {
+
+                if (validPixel(row, col, v0, v1, v2)) {
+                    canvas.setPixelColor(row, col, c);
+                }
+
+
+            }
+        }
+    }
+
     
 }
 
+
+//returns the area of a triangle define by three points
+float area(int x1, int y1, int x2, int y2, int x3, int y3) {
+
+    return abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0);
+
+}
+
+//test in the point (x,y) is inside the triandle define by v0,v1 and v2
+bool validPixel(int x, int y, Vec2 v0, Vec2 v1, Vec2 v2) {
+
+
+    float mainArea = area(v0.x, v0.y, v1.x, v1.y, v2.x, v2.y);
+
+    float a1 = area(x, y, v1.x, v1.y, v2.x, v2.y);
+
+    float a2 = area(v0.x, v0.y, x, y, v2.x, v2.y);
+
+    float a3 = area(v0.x, v0.y, v1.x, v1.y, x, y);
+
+    return (mainArea == a1 + a2 + a3);
+
+}
 
 // Main
 int main(){
@@ -115,13 +165,13 @@ int main(){
     Vec2 line[2] = {Vec2(0,0), Vec2(100,100)};
 
     // Set the fill mode
-    glPolygonMode(LINE);
+    glPolygonMode(FILL);
 
     // Draw a line
     drawLine(line[0],line[1],canvas,red);
 
     // Data for our triangle
-    Vec2 tri[3] = {Vec2(160,60),Vec2(150,10),Vec2(75,190)};
+    Vec2 tri[3] = {Vec2(160,60),Vec2(15,50),Vec2(75,190)};
 
     // Draw a triangle
     triangle(tri[0],tri[1],tri[2],canvas,red);

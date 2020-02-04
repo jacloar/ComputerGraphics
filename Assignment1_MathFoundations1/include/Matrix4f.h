c4b08ea9cc924e669539c22483d0a0bb221ce4f3
Jack Loar <jacloar98@gmail.com>
2020-02-03 22:13:32 -0500

    assignment 1 - unit tests

--- Assignment1_MathFoundations1/include/Matrix4f.h
+++ Assignment1_MathFoundations1/include/Matrix4f.h
@@ -107,10 +107,23 @@ public:
 
 };
 
+// Matrix multiply by a vector
+
+Vector4f operator *(const Matrix4f& M, const Vector4f& v) {
+
+    Vector4f vec;
+    vec.x = Dot(Vector4f(M(0, 0), M(0, 1), M(0, 2), M(0, 3)), v);
+    vec.y = Dot(Vector4f(M(1, 0), M(1, 1), M(1, 2), M(1, 3)), v);
+    vec.z = Dot(Vector4f(M(2, 0), M(2, 1), M(2, 2), M(2, 3)), v);
+    vec.w = Dot(Vector4f(M(3, 0), M(3, 1), M(3, 2), M(3, 3)), v);
+
+    return vec;
+}
+
 // Matrix Multiplication
 Matrix4f operator *(const Matrix4f& A, const Matrix4f& B){
    
-  Vector4f col1 (B(0,0), B(1,0), B(2,0), B(3,0));
+  Vector4f col1 (B(0, 0), B(1, 0), B(2, 0), B(3, 0));
   Vector4f col2 (B(0, 1), B(1, 1), B(2, 1), B(3, 1));
   Vector4f col3 (B(0, 2), B(1, 2), B(2, 2), B(3, 2));
   Vector4f col4 (B(0, 3), B(1, 3), B(2, 3), B(3, 3));
@@ -118,19 +131,6 @@ Matrix4f operator *(const Matrix4f& A, const Matrix4f& B){
   Matrix4f mat4(A*col1, A*col2, A*col3, A*col4);
 
   return mat4;
-}
-
-// Matrix multiply by a vector
-
-Vector4f operator *(const Matrix4f& M, const Vector4f& v){
-  
-    Vector4f vec;
-    vec.x = Dot(Vector4f(M(0, 0), M(0, 1), M(0, 2), M(0, 3)), v);
-    vec.y = Dot(Vector4f(M(1, 0), M(1, 1), M(1, 2), M(1, 3)), v);
-    vec.z = Dot(Vector4f(M(2, 0), M(2, 1), M(2, 2), M(2, 3)), v);
-    vec.w = Dot(Vector4f(M(3, 0), M(3, 1), M(3, 2), M(3, 3)), v);
-
-  return vec;
 }
 
 
--- Assignment1_MathFoundations1/src/main.cpp
+++ Assignment1_MathFoundations1/src/main.cpp
@@ -104,33 +104,32 @@ bool unitTest2(){
 }
 
 // Sample unit test comparing against GLM.
-// TODO: Test against glm::scale
 bool unitTest3(){
-	glm::mat4 glmScale = glm::mat4(2.0f);
+	glm::mat4 glmScale = glm::scale(glm::vec3(2.0f));
 	Vector4f a(1.0f,0,0,0);
-	Vector4f b(0.0f,1.0f,0,0);
+	Vector4f b(0,1.0f,0,0);
 	Vector4f c(0,0,1.0f,0);
 	Vector4f d(0,0,0,1.0f);
 	Matrix4f myScaled(a,b,c,d);
-    myScaled.MakeScale(2.0f,2.0f,2.0f);
+    myScaled = myScaled * myScaled.MakeScale(2.0f,2.0f,2.0f);
 
     if(
-        glmScale[0][0]==myScaled[0][0] &&
-        glmScale[0][1]==myScaled[0][1] &&
-        glmScale[0][2]==myScaled[0][2] &&
-        glmScale[0][3]==myScaled[0][3] &&
-        glmScale[1][0]==myScaled[1][0] &&
-        glmScale[1][1]==myScaled[1][1] &&
-        glmScale[1][2]==myScaled[1][2] &&
-        glmScale[1][3]==myScaled[1][3] &&
-        glmScale[2][0]==myScaled[2][0] &&
-        glmScale[2][1]==myScaled[2][1] &&
-        glmScale[2][2]==myScaled[2][2] &&
-        glmScale[2][3]==myScaled[2][3] &&
-        glmScale[3][0]==myScaled[3][0] &&
-        glmScale[3][1]==myScaled[3][1] &&
-        glmScale[3][2]==myScaled[3][2] &&
-        glmScale[3][3]==myScaled[3][3]){
+        glmScale[0].x==myScaled[0].x &&
+        glmScale[0].y==myScaled[0].y &&
+        glmScale[0].z==myScaled[0].z &&
+        glmScale[0].w==myScaled[0].w &&
+        glmScale[1].x == myScaled[1].x &&
+        glmScale[1].y == myScaled[1].y &&
+        glmScale[1].z == myScaled[1].z &&
+        glmScale[1].w == myScaled[1].w &&
+        glmScale[2].x == myScaled[2].x &&
+        glmScale[2].y == myScaled[2].y &&
+        glmScale[2].z == myScaled[2].z &&
+        glmScale[2].w == myScaled[2].w &&
+        glmScale[3].x == myScaled[3].x &&
+        glmScale[3].y == myScaled[3].y &&
+        glmScale[3].z == myScaled[3].z &&
+        glmScale[3].w == myScaled[3].w){
             return true;
     }
 	
@@ -172,6 +171,105 @@ bool unitTest5(){
     return false;
 }
 
+// Sample unit test comparing against GLM.
+bool unitTest6() {
+    glm::mat4 glmScale = glm::rotate(2.0f, glm::vec3(1.0f, 0, 0));
+    Vector4f a(1.0f, 0, 0, 0);
+    Vector4f b(0, 1.0f, 0, 0);
+    Vector4f c(0, 0, 1.0f, 0);
+    Vector4f d(0, 0, 0, 1.0f);
+    Matrix4f myRotated(a, b, c, d);
+    myRotated = myRotated * myRotated.MakeRotationX(2.0f);
+
+    if (
+        glmScale[0].x == myRotated[0].x &&
+        glmScale[0].y == myRotated[0].y &&
+        glmScale[0].z == myRotated[0].z &&
+        glmScale[0].w == myRotated[0].w &&
+        glmScale[1].x == myRotated[1].x &&
+        glmScale[1].y == myRotated[1].y &&
+        glmScale[1].z == myRotated[1].z &&
+        glmScale[1].w == myRotated[1].w &&
+        glmScale[2].x == myRotated[2].x &&
+        glmScale[2].y == myRotated[2].y &&
+        glmScale[2].z == myRotated[2].z &&
+        glmScale[2].w == myRotated[2].w &&
+        glmScale[3].x == myRotated[3].x &&
+        glmScale[3].y == myRotated[3].y &&
+        glmScale[3].z == myRotated[3].z &&
+        glmScale[3].w == myRotated[3].w) {
+        return true;
+    }
+
+    return false;
+}
+
+// Sample unit test comparing against GLM.
+bool unitTest7() {
+    glm::mat4 glmScale = glm::rotate(2.0f, glm::vec3(0, 1.0f, 0));
+    Vector4f a(1.0f, 0, 0, 0);
+    Vector4f b(0, 1.0f, 0, 0);
+    Vector4f c(0, 0, 1.0f, 0);
+    Vector4f d(0, 0, 0, 1.0f);
+    Matrix4f myRotated(a, b, c, d);
+    myRotated = myRotated * myRotated.MakeRotationY(2.0f);
+
+    if (
+        glmScale[0].x == myRotated[0].x &&
+        glmScale[0].y == myRotated[0].y &&
+        glmScale[0].z == myRotated[0].z &&
+        glmScale[0].w == myRotated[0].w &&
+        glmScale[1].x == myRotated[1].x &&
+        glmScale[1].y == myRotated[1].y &&
+        glmScale[1].z == myRotated[1].z &&
+        glmScale[1].w == myRotated[1].w &&
+        glmScale[2].x == myRotated[2].x &&
+        glmScale[2].y == myRotated[2].y &&
+        glmScale[2].z == myRotated[2].z &&
+        glmScale[2].w == myRotated[2].w &&
+        glmScale[3].x == myRotated[3].x &&
+        glmScale[3].y == myRotated[3].y &&
+        glmScale[3].z == myRotated[3].z &&
+        glmScale[3].w == myRotated[3].w) {
+        return true;
+    }
+
+    return false;
+}
+
+// Sample unit test comparing against GLM.
+bool unitTest8() {
+    glm::mat4 glmScale = glm::rotate(2.0f, glm::vec3(0, 0, 1.0f));
+    Vector4f a(1.0f, 0, 0, 0);
+    Vector4f b(0, 1.0f, 0, 0);
+    Vector4f c(0, 0, 1.0f, 0);
+    Vector4f d(0, 0, 0, 1.0f);
+    Matrix4f myRotated(a, b, c, d);
+    myRotated = myRotated * myRotated.MakeRotationZ(2.0f);
+
+    if (
+        glmScale[0].x == myRotated[0].x &&
+        glmScale[0].y == myRotated[0].y &&
+        glmScale[0].z == myRotated[0].z &&
+        glmScale[0].w == myRotated[0].w &&
+        glmScale[1].x == myRotated[1].x &&
+        glmScale[1].y == myRotated[1].y &&
+        glmScale[1].z == myRotated[1].z &&
+        glmScale[1].w == myRotated[1].w &&
+        glmScale[2].x == myRotated[2].x &&
+        glmScale[2].y == myRotated[2].y &&
+        glmScale[2].z == myRotated[2].z &&
+        glmScale[2].w == myRotated[2].w &&
+        glmScale[3].x == myRotated[3].x &&
+        glmScale[3].y == myRotated[3].y &&
+        glmScale[3].z == myRotated[3].z &&
+        glmScale[3].w == myRotated[3].w) {
+        return true;
+    }
+
+    return false;
+}
+
 int main(){
     // Keep track of the tests passed
     unsigned int testsPassed = 0;
@@ -183,6 +281,9 @@ int main(){
     std::cout << "Passed 3: " << unitTest3() << " \n";
     std::cout << "Passed 4: " << unitTest4() << " \n";
     std::cout << "Passed 5: " << unitTest5() << " \n";
+    std::cout << "Passed 6: " << unitTest6() << " \n";
+    std::cout << "Passed 7: " << unitTest7() << " \n";
+    std::cout << "Passed 8: " << unitTest8() << " \n";
 
     return 0;
 }
